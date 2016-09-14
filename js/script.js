0a780936821b098eefb01fbf4b2a79997a53febb
Linse Mathew <linsemathew123@gmail.com>
2016-09-14 05:43:55 -0400

    Add game logic for ties.

--- js/script.js
+++ js/script.js
@@ -13,34 +13,41 @@ $(document).ready(function(){
 	var spot9 = $('#spot9');
 
 	$('#board li').on('click', function(){
-		if(spot1.hasclass('O') && spot2.hasclass('O') spot3.hasclass('O') ||
-			spot4.hasclass('O') && spot5.hasclass('O') spot6.hasclass('O') ||
-			spot7.hasclass('O') && spot8.hasclass('O') spot9.hasclass('O') ||
-			spot1.hasclass('O') && spot4.hasclass('O') spot7.hasclass('O') ||
-			spot2.hasclass('O') && spot5.hasclass('O') spot8.hasclass('O') ||
-			spot3.hasclass('O') && spot6.hasclass('O') spot9.hasclass('O') ||
-			spot1.hasclass('O') && spot5.hasclass('O') spot9.hasclass('O') ||
-			spot3.hasclass('O') && spot5.hasclass('O') spot7.hasclass('O') ||
+		if(spot1.hasclass('O') && spot2.hasclass('O') && spot3.hasclass('O') ||
+			spot4.hasclass('O') && spot5.hasclass('O') && spot6.hasclass('O') ||
+			spot7.hasclass('O') && spot8.hasclass('O') && spot9.hasclass('O') ||
+			spot1.hasclass('O') && spot4.hasclass('O') && spot7.hasclass('O') ||
+			spot2.hasclass('O') && spot5.hasclass('O') && spot8.hasclass('O') ||
+			spot3.hasclass('O') && spot6.hasclass('O') && spot9.hasclass('O') ||
+			spot1.hasclass('O') && spot5.hasclass('O') && spot9.hasclass('O') ||
+			spot3.hasclass('O') && spot5.hasclass('O') && spot7.hasclass('O')
 		){
 			alert("X is the winner!");
 			$('#board li').text(' ');
 			$('#board li').removeClass('disable');
 			$('#board li').removeClass('O');
 			$('#board li').removeClass('X');
-		} else if(spot1.hasclass('X') && spot2.hasclass('X') spot3.hasclass('X') ||
-			spot4.hasclass('X') && spot5.hasclass('X') spot6.hasclass('X') ||
-			spot7.hasclass('X') && spot8.hasclass('X') spot9.hasclass('X') ||
-			spot1.hasclass('X') && spot4.hasclass('X') spot7.hasclass('X') ||
-			spot2.hasclass('X') && spot5.hasclass('X') spot8.hasclass('X') ||
-			spot3.hasclass('X') && spot6.hasclass('X') spot9.hasclass('X') ||
-			spot1.hasclass('X') && spot5.hasclass('X') spot9.hasclass('X') ||
-			spot3.hasclass('X') && spot5.hasclass('X') spot7.hasclass('X') ||
+		} else if(spot1.hasclass('X') && spot2.hasclass('X') && spot3.hasclass('X') ||
+			spot4.hasclass('X') && spot5.hasclass('X') && spot6.hasclass('X') ||
+			spot7.hasclass('X') && spot8.hasclass('X') && spot9.hasclass('X') ||
+			spot1.hasclass('X') && spot4.hasclass('X') && spot7.hasclass('X') ||
+			spot2.hasclass('X') && spot5.hasclass('X') && spot8.hasclass('X') ||
+			spot3.hasclass('X') && spot6.hasclass('X') && spot9.hasclass('X') ||
+			spot1.hasclass('X') && spot5.hasclass('X') && spot9.hasclass('X') ||
+			spot3.hasclass('X') && spot5.hasclass('X') && spot7.hasclass('X') 
 		){
 			alert("X is the winner!");
 			$('#board li').text();
 			$('#board li').removeClass('disable');
 			$('#board li').removeClass('O');
 			$('#board li').removeClass('X');
+		} else if(turns == 9){
+			alert("Tie Game!");
+			$('#board li').text();
+			$('#board li').removeClass('disable');
+			$('#board li').removeClass('O');
+			$('#board li').removeClass('X');
+			turns = 0;
 		}
 	});
 });
